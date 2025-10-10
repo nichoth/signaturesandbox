a3d2b7e28f5b48d54b28c33a1830f6bbf2e4fed7
nichoth <nichoth@nichoth.com>
2025-10-09 20:16:52 -0700

    use em dash

--- src/routes/home.ts
+++ src/routes/home.ts
@@ -1,6 +1,6 @@
 import { html } from 'htm/preact'
 import { FunctionComponent } from 'preact'
-import { ELLIPSIS } from '../constants.js'
+import { ELLIPSIS, EM_DASH } from '../constants.js'
 
 export const HomeRoute:FunctionComponent = function HomeRoute () {
     return html`<div class="route home">
@@ -13,8 +13,8 @@ export const HomeRoute:FunctionComponent = function HomeRoute () {
 
         <p>
             This uses the <a href="https://developer.mozilla.org/en-US/docs/Web/API/Web_Crypto_API">
-            Web Crypto API</a> to sign and verify things in your browser;
-            no servers required.
+            Web Crypto API</a> to sign and verify things in your browser
+            ${' ' + EM_DASH} no servers required.
         </p>
 
         <nav class="signature-nav">
